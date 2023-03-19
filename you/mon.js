af2faa536015d2108408b70f3652d152b022a5e5
youdonghee <0103donghee@naver.com>
2023-03-19 21:21:58 +0900

    1

--- you/mon.js
+++ you/mon.js
@@ -1,8 +1,20 @@
 let userman;
 let dieMonster=[];
+let monsterArr=[];
 let mainmonster;
 let outputdiv = document.querySelector(".monster-output");
 // 캐릭터 생성자함수
+
+let nums = [0,1,2,3,4,5,6,7,8,9]
+console.log(nums.splice(3,2));
+console.log(nums);
+
+
+
+monsterSetup();
+
+console.log(monsterArr);
+
 function user(number){
   if(number == 1){
     this.name = "player1"; //공격력 쎈 애
@@ -42,18 +54,19 @@ function monsterSelect(){
   outputdiv.innerHTML = `몬스터이름: ${mainmonster.name} 몬스터HP: ${mainmonster.hp} 몬스터 공격력: ${mainmonster.atk}  몬스터 방격력: ${mainmonster.def} 몬스터랜덤박스: ${mainmonster.box}`
 }
 
-// 몬스터 소환 (이름, hp, atk, def, exp, 돈)
-let monsterArr=[];
-monsterArr.push(new monster("몬스터1", 20, 10, 10, 5, 1000))
-monsterArr.push(new monster("몬스터2", 40, 15, 15, 10, 1000))
-monsterArr.push(new monster("몬스터3", 30, 20, 20, 15, 1000))
-monsterArr.push(new monster("몬스터4", 45, 25, 25, 20, 1000))
-monsterArr.push(new monster("몬스터5", 50, 30, 25, 25, 1000))
-monsterArr.push(new monster("몬스터6", 70, 30, 30, 30, 1000))
-monsterArr.push(new monster("몬스터7", 80, 30, 30, 35, 1000))
-monsterArr.push(new monster("몬스터8", 90, 35, 30, 40, 1000))
-monsterArr.push(new monster("몬스터9", 100, 35, 30, 40, 1000))
-monsterArr.push(new monster("보스몬", 150, 50, 10, 100, 1000))
+function monsterSetup(){
+  monsterArr[0]= new monster("몬스터1", 20, 10, 10, 5, 1000)
+  monsterArr[1]= new monster("몬스터2", 40, 15, 15, 10, 1000)
+  monsterArr[2]= new monster("몬스터3", 30, 20, 20, 15, 1000)
+  monsterArr[3]= new monster("몬스터4", 45, 25, 25, 20, 1000)
+  monsterArr[4]= new monster("몬스터5", 50, 30, 25, 25, 1000)
+  monsterArr[5]= new monster("몬스터6", 70, 30, 30, 30, 1000)
+  monsterArr[6]= new monster("몬스터7", 80, 30, 30, 35, 1000)
+  monsterArr[7]= new monster("몬스터8", 90, 35, 30, 40, 1000)
+  monsterArr[8]= new monster("몬스터9", 100, 35, 30, 40, 1000)
+  monsterArr[9]= new monster("보스몬", 150, 50, 10, 100, 1000)
+}
+
 // monsterArr[0]= new monster("몬스터1", 20, 30, 10, 20); //체력 20 
 // monsterArr[1]= new monster("몬스터2", 40, 20, 30, 20); //체력 40 
 
@@ -61,15 +74,17 @@ monsterArr.push(new monster("보스몬", 150, 50, 10, 100, 1000))
 function monsterChoice(){
   
   let randomIndex = Math.floor(Math.random()* monsterArr.length);
-  for (let i = 0; i < monsterArr.length; i++) {
-    switch (randomIndex){
-      case i:
-          mainmonster = monsterArr[i];
-          box();
-        break;
-      default:
-        break;
-    }
+  if(mainmonster == null){
+    for (let i = 0; i < monsterArr.length; i++) {
+        switch (randomIndex){
+          case i:
+              mainmonster = monsterArr[i];
+              box();
+            break;
+          default:
+            break;
+        }
+  }
   }
   outputdiv.innerHTML = `몬스터이름: ${mainmonster.name} 몬스터HP: ${mainmonster.hp} 몬스터 공격력: ${mainmonster.atk}  몬스터 방격력: ${mainmonster.def} 몬스터랜덤박스: ${mainmonster.box}`
 }
@@ -110,6 +125,7 @@ function expFun(exp,money){
   userman.exp += exp;
   userman.money += money;
   dieMonster.push("잔해물")
+  dieMonster.push(mainmonster.box);
   if(userman.level * 30 <= userman.exp){
     userman.exp -= userman.level * 30;
     userman.level += 1;
@@ -118,6 +134,7 @@ function expFun(exp,money){
     // userman.hp += 50;
   }
   userselect(userman);
+  console.log(dieMonster);
 }
 
 // 플레이어가 몬스터 공격하기
@@ -134,15 +151,20 @@ attack.onclick = function(){
     alert(`당신은 ${mainmonster.name}를 처치했다`);
     document.querySelector(".msg").innerHTML= ""
     expFun(mainmonster.exp, mainmonster.money);
+    console.log(monsterArr);
     mainmonster = null;
     outputdiv.innerHTML = mainmonster;
+    monsterSetup();
   }
 }
 // 몬스터가 플레이어 공격하기
-// 함수
+// 몬스터 피해 입혔을때
+// 몬스터 죽였을때 값 초기화
 
-// 이미지 온클릭 -> 클릭하면 함수 발동
+// 죽었을때 박스 떨어지기 -> 완료
 
+// 오류 : 몬스터 처치하면 다시 hp 리셋 -> 함수 만들어서 공격값 밑에 리셋
 
-// 오류 : 몬스터 처치하면 다시 hp 리셋 / 때리고 도망가기
-// 플레이어 죽으면 어떻게 할건지 -> 게임 오버
+// 오류 : 몬스터 처치하면 다시 hp 리셋
+// 때리고 도망가기
+// 플레이어 죽으면 어떻게 할건지~
